2879884e0d2daa8e32d8ac36869651799dba9c28
kimk3361 <55775127+kimk3361@users.noreply.github.com>
2019-09-24 23:16:29 -0400

    Add files via upload
    First Submission

---
+++ P2T1_SalesPrediction_Kim.py
@@ -0,0 +1,8 @@
+# A company has determined that its annual profit is typically 23 percent of total sales. Write program that asks user to enter projected amount of total sales, then displays the profit that will be made from the amount
+# September 24, 2019
+# CTI-110 P2T1 - Sales Prediction
+# Kihan Kim
+#
+total_sales = float(input('What is the projected amount of total sales? '))
+annual_profit = total_sales * 0.23
+print('The annual profit from total sales is', annual_profit)
